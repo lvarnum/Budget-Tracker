43ab76d3025f1c3d32c8414f7f3f76488913de76
lvarnum <lvarnum@email.arizona.edu>
2020-06-17 22:32:03 -0700

    Updates

---
+++ precache-manifest.1938acda798fbf177ebed4c048fce19d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e828990deb3aa6fda3e4f33415856d",
+    "url": "/Budget-Tracker/index.html"
+  },
+  {
+    "revision": "a7900d526cb32cee0253",
+    "url": "/Budget-Tracker/static/js/2.fc5c7591.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-Tracker/static/js/2.fc5c7591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c527161fb130869ba1a",
+    "url": "/Budget-Tracker/static/js/main.2d5a332a.chunk.js"
+  },
+  {
+    "revision": "64414252493a03e710e8",
+    "url": "/Budget-Tracker/static/js/runtime-main.f58261fb.js"
+  }
+]);
